f9bf8a37819e790a4e52c680d19b8b49a293a183
Dean <deanlt.santos@gmail.com>
2026-07-29 10:38:25 +0800

    feat(question): add endpoint to get all questions

--- src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
+++ src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
@@ -1,14 +1,23 @@
-package com.github.dltsntos.quizapp;
+package com.github.dltsntos.quizapp.controller;
 
+import com.github.dltsntos.quizapp.model.Question;
+import com.github.dltsntos.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("question")
+
 public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestions(){
+    public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 }
