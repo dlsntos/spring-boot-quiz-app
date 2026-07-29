ffe36d24f3a217f725311103c1b558e713444bb0
Dean <deanlt.santos@gmail.com>
2026-07-29 14:08:53 +0800

    refactor: implement Response Entity for proper Http Status codes and exception handling

--- src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
+++ src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.github.dltsntos.quizapp.controller;
 import com.github.dltsntos.quizapp.model.Question;
 import com.github.dltsntos.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +16,17 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getAllQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getAllQuestionsByCategory(@PathVariable String category){
         return questionService.getAllQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 }
--- src/main/java/com/github/dltsntos/quizapp/service/QuestionService.java
+++ src/main/java/com/github/dltsntos/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.github.dltsntos.quizapp.service;
 import com.github.dltsntos.quizapp.dao.QuestionDao;
 import com.github.dltsntos.quizapp.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -13,18 +16,28 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
 
-    public List<Question> getAllQuestionsByCategory(String category) {
-        return  questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getAllQuestionsByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
 
-        return "success";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 }
