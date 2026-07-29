53ede96d3b5432f604e8bbac5967e7994e492c11
Dean <deanlt.santos@gmail.com>
2026-07-29 10:06:21 +0800

    feat(question): add new question model

--- src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
+++ src/main/java/com/github/dltsntos/quizapp/controller/QuestionController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
     @GetMapping("allQuestions")
     public String getAllQuestions(){
-        return "This is a question";
+        return questionService.getAllQuestions();
     }
 }
